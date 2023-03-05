562e454bc51f7053242d29e8c00516ddbed43682
zFranco23 <francoheflo@gmail.com>
2023-03-05 18:45:29 -0500

    Work implemetation

--- src/components/ChatContainer/ChatContainer.tsx
+++ src/components/ChatContainer/ChatContainer.tsx
@@ -1,5 +1,5 @@
 
-import { FC, useCallback, useEffect, useState } from "react"
+import { FC, useCallback, useEffect, useRef, useState } from "react"
 import { getMessages } from "../../helpers";
 import Message from "../Message/Message";
 
@@ -9,16 +9,29 @@ import style from './style.module.css';
 
 const ChatContainer: FC<Props> = (props) => {
 
+  const messageListRef = useRef<HTMLDivElement | null>(null)
+  const hiddenDivRef = useRef<HTMLDivElement | null>(null)
+  const loaderRef = useRef<HTMLDivElement | null>(null)
+  const [showObserver,setShowObserver] = useState<boolean>(true);
   const [page,setPage] = useState<number>(0); 
   const [isLoading,setIsLoading] = useState<boolean>(false);
   const [messages,setMessages]= useState<Array<any>>([]) 
 
 
   const fetchMessages = useCallback(async ()=>{
+    console.log(page);
+    setShowObserver(false);
     setIsLoading(true)
-    const data = await getMessages(page);
-    console.log(data);
-    
+    const data = await getMessages(page);    
+    if(messageListRef.current){
+        console.log( messageListRef.current.firstChild);
+        ;
+        const elem = messageListRef.current;
+        console.log(messageListRef.current.offsetTop);
+        
+        elem.scrollTop = 100
+    }
+    setShowObserver(true);
     setMessages(ps => [...data,...ps])
     setIsLoading(false);
   },[page])
@@ -28,11 +41,52 @@ const ChatContainer: FC<Props> = (props) => {
   },[fetchMessages])
 
 
+  useEffect(()=>{
+    if(messages.length && page === 0){
+        const elem = messageListRef.current;
+
+        if(elem){
+            elem.scrollTop = elem.scrollHeight
+        }
+    }
+  },[messages, page])
+
+
+
+  const observerCallback = useCallback((observerEntries: any) => {
+    if (observerEntries[0].isIntersecting) {
+        if(!isLoading){
+            setPage( ps => ps + 1)
+        }
+      }
+  },[isLoading])
+
+  useEffect(()=>{
+    let observer = new IntersectionObserver(observerCallback, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0
+    });
+    
+        if(hiddenDivRef.current){
+            observer.observe(hiddenDivRef.current)
+        }
+    
+    
+        return () => {
+            if(hiddenDivRef.current){
+                observer.unobserve(hiddenDivRef.current);
+            }
+        }
+  },[observerCallback])
+
   return (
     <div className={style.chatContainer} id="scrollArea">
-        {isLoading && <div>...loading </div>}
+
         
-        <div className={style.messagesList}>
+        <div className={style.messagesList} ref={messageListRef}>
+        {isLoading && <div>...loading </div>}
+            {showObserver && <div ref={hiddenDivRef}></div>}
             {messages.map((m, idx: number)=> (
                 <Message key={`message-${idx}`} message={m} />
             ))}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,11 +2,11 @@ import { MOCK_DATA_MESSAGES } from "../constants"
 import { Message } from "../types";
 
 
-export const getMessages = async (page: number, size: number = 5 ) => {
+export const getMessages = async (page: number, size: number = 15 ) => {
 
     return new Promise<Message[]>((resolve,reject) => {
         setTimeout(() => {
           resolve(MOCK_DATA_MESSAGES.slice(page, page + size));
-        }, 1000);
+        }, 2000);
       });
 }
